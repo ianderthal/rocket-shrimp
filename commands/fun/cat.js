5defdc35a3b7385714e5d9daa24c8b661429f19f
Ian Gallardo <gallard3@gmail.com>
2023-09-17 21:24:11 -0400

    API call to urbandictionary

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -4,10 +4,16 @@ const { request } = require('undici');
 module.exports = {
     data: new SlashCommandBuilder ()
         .setName('cat')
-        .setDescription('fetches a random cat image'),
+        .setDescription('fetches a random definition of a cat'),
     async execute(interaction) {
-        const catResult = await request('https://cataas.com/cat');
-        await interaction.reply({ content:'Fetching a cat right meow!', fetchReply: true });
-        console.log('response received', catResult.statusCode);
+        const catResult = await request('https://api.urbandictionary.com/v0/define?term=cat');
+        await interaction.reply({ content:'Fetching the definition of a cat right meow!', fetchReply: true });
+
+        const { list } = await catResult.body.json();
+        //console.log('data', await catResult.body.json());
+        if (!list.length) {
+		    return interaction.editReply(`No results found for your request.`);
+		}
+        interaction.editReply(`**Definition of a cat**: ${list[0].definition}`);
     },
 };
